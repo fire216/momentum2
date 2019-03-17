d29855f59165e384006f082c31972674d2d24f47
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-03-17 23:02:04 +0900

    momentum prototype finish

--- weather.js
+++ weather.js
@@ -1,5 +1,23 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "0b17068d862bcf8e02621bbb158c4bc6";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +31,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,7 +47,8 @@ function loadedCoords() {
     if(loadedCoords === null) {
         askForCoords();
     } else {
-        //getWeather
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
